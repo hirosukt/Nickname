26f7268b0ee6b8a427361f84307bc42d63d07210
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-10-02 16:26:20 +0900

    1.0.1

--- src/main/java/love/chihuyu/commands/CommandUnnick.kt
+++ src/main/java/love/chihuyu/commands/CommandUnnick.kt
@@ -3,6 +3,7 @@ package love.chihuyu.commands
 import dev.jorel.commandapi.CommandAPICommand
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
 import love.chihuyu.Plugin.Companion.plugin
+import love.chihuyu.utils.NickUtil
 import xyz.haoshoku.nick.api.NickAPI
 
 object CommandUnnick {
@@ -11,19 +12,7 @@ object CommandUnnick {
         .withPermission("nickname.unnick")
         .executesPlayer(PlayerCommandExecutor { sender, args ->
             val originalName = NickAPI.getOriginalGameProfileName(sender)
-            plugin.server.onlinePlayers.forEach { it.hidePlayer(plugin, sender) }
-
-            sender.setPlayerListName(originalName)
-            sender.setDisplayName(originalName)
-
-            sender.customName = originalName
-            sender.isCustomNameVisible = true
-
-            NickAPI.resetNick(sender)
-
-            plugin.config.set(sender.uniqueId.toString(), originalName)
-            plugin.saveConfig()
-            plugin.server.onlinePlayers.forEach { it.showPlayer(plugin, sender) }
+            NickUtil.setDisplayNick(sender, originalName)
 
             sender.sendMessage("Nickname reset.")
         })
